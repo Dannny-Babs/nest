9ac1910ab76cd39d05de3562216d25f68cec924b
Daniel Babalola <dannybabs123456@gmail.com>
2025-06-17 16:54:16 -0400

    Enhance layout and home screen. Updated RootLayout to include a splash screen and modified the HomeScreen component for improved styling and content.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+
+import "../global.css";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
